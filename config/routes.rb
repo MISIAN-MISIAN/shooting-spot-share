5e9bce4c89bffb6b5f34a74149d81bcaedddec8e
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2019-11-20 17:14:27 +0900

    ツイート機能実装
    ツイートのみ
    imageはまだ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: "tweets#index"
-  # resources :tweets, only: :index
+  # root to: "tweets#index"
+  resources :tweets, only: [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
