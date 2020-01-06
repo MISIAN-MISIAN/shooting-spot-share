81f31fa28c427964328d10b44349f1847af0cf3d
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2020-01-06 20:02:22 +0900

    修正
    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'shooting-spot-share'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:MSIAN-MISIAN/shooting-spot-share.git'
+set :repo_url,  'git@github.com:MISIAN-MISIAN/shooting-spot-share.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
