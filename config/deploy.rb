9bc147becd35c3a6ff36cef760bc9f064b872ffa
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2020-01-09 13:56:44 +0900

    Capistrano
    追記

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,6 +17,8 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/myhomepage.pem'] 
+# secrets.yml用のシンボリックリンクを追加
+set :linked_files, %w{ config/secrets.yml }
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
